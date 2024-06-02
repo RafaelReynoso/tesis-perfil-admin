c2aa458878827bb8d0ae6f5fa45f9167f36baf35
RafaelReynoso <rafael.reynoso@tecsup.edu.pe>
2024-06-02 18:13:40 -0500

    actualizando notificaciones y conductores

--- lib/pages/conductores.dart
+++ lib/pages/conductores.dart
@@ -1,10 +1,253 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_database/firebase_database.dart';
 
-class Conductores extends StatelessWidget {
+class Conductores extends StatefulWidget {
   const Conductores({super.key});
 
   @override
+  State<Conductores> createState() => _ConductoresState();
+}
+
+class _ConductoresState extends State<Conductores> {
+  final DatabaseReference _conductoresRef =
+      FirebaseDatabase.instance.ref().child('user_locations/conductores');
+
+  final TextEditingController _nombreController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _placaController = TextEditingController();
+
+  @override
   Widget build(BuildContext context) {
-    return const Text('Hello, World!');
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Conductores', style: TextStyle(color: Colors.green, fontWeight: FontWeight.bold,),),
+      ),
+      body: FutureBuilder<DataSnapshot>(
+        future: _conductoresRef.once().then((event) => event.snapshot),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return const Center(child: Text('Error al cargar los datos'));
+          } else if (!snapshot.hasData) {
+            return const Center(child: Text('No hay datos disponibles'));
+          }
+
+          // Procesa los datos de los conductores
+          final conductoresData = snapshot.data!.value as Map<dynamic, dynamic>;
+
+          return ListView.builder(
+            
+            itemCount: conductoresData.length,
+            itemBuilder: (context, index) {
+              final conductorKey = conductoresData.keys.elementAt(index);
+              final conductorInfo =
+                  conductoresData[conductorKey]['informacion'];
+
+              return Card(
+                child: ListTile(
+                  title: Text(conductorKey),
+                  subtitle: Text('Placa: ${conductorInfo['placa']}'),
+                  trailing: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      IconButton(
+                        icon: const Icon(Icons.edit),
+                        onPressed: () {
+                          _mostrarEditarConductorDialog(
+                              conductorKey, conductorInfo);
+                        },
+                      ),
+                      IconButton(
+                        icon: const Icon(Icons.delete),
+                        onPressed: () {
+                          _eliminarConductor(conductorKey);
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            },
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        foregroundColor: Colors.green,
+        backgroundColor: Colors.white,
+        onPressed: () {
+          // Mostrar un diálogo para agregar un nuevo conductor
+          showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return _buildAgregarConductorDialog(context);
+            },
+          );
+        },
+        child: const Icon(Icons.add, color: Colors.green,), // Icono de agregar
+      ),
+    );
+  }
+
+  Widget _buildAgregarConductorDialog(BuildContext context) {
+    return AlertDialog(
+      title: const Text('Agregar Conductor'),
+      content: SingleChildScrollView(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            TextField(
+              controller: _nombreController,
+              decoration: const InputDecoration(labelText: 'Nombre'),
+            ),
+            TextField(
+              controller: _passwordController,
+              decoration: const InputDecoration(labelText: 'Contraseña'),
+              obscureText: true,
+            ),
+            TextField(
+              controller: _placaController,
+              decoration: const InputDecoration(labelText: 'Placa'),
+            ),
+          ],
+        ),
+      ),
+      actions: [
+        TextButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          child: const Text('Cancelar'),
+        ),
+        ElevatedButton(
+          onPressed: () {
+            _crearConductor();
+            Navigator.of(context).pop(); // Cierra el diálogo
+          },
+          child: const Text('Agregar'),
+        ),
+      ],
+    );
+  }
+
+  void _crearConductor() {
+    String nombre = _nombreController.text.trim();
+    String password = _passwordController.text.trim();
+    String placa = _placaController.text.trim();
+
+    if (nombre.isNotEmpty && password.isNotEmpty && placa.isNotEmpty) {
+      _conductoresRef.child(nombre).set({
+        'informacion': {
+          'password': password,
+          'placa': placa,
+        }
+      }).then((_) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('Conductor agregado exitosamente')),
+        );
+        _nombreController.clear();
+        _passwordController.clear();
+        _placaController.clear();
+        // Actualizar la lista después de agregar un nuevo conductor
+        setState(() {});
+      }).catchError((error) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Error al agregar conductor: $error')),
+        );
+      });
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Por favor completa todos los campos')),
+      );
+    }
+  }
+
+  void _mostrarEditarConductorDialog(String nombre, Map conductorInfo) {
+    _nombreController.text = nombre;
+    _passwordController.text = conductorInfo['password'];
+    _placaController.text = conductorInfo['placa'];
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Editar Conductor'),
+          content: SingleChildScrollView(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                TextField(
+                  controller: _nombreController,
+                  decoration: const InputDecoration(labelText: 'Nombre'),
+                  enabled: false, // Deshabilita la edición del nombre
+                ),
+                TextField(
+                  controller: _passwordController,
+                  decoration: const InputDecoration(labelText: 'Contraseña'),
+                  obscureText: true,
+                ),
+                TextField(
+                  controller: _placaController,
+                  decoration: const InputDecoration(labelText: 'Placa'),
+                ),
+              ],
+            ),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('Cancelar'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                _actualizarConductor(
+                  _nombreController.text.trim(),
+                  _passwordController.text.trim(),
+                  _placaController.text.trim(),
+                );
+                Navigator.of(context).pop();
+              },
+              child: const Text('Guardar'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _actualizarConductor(String nombre, String newPassword, String newPlaca) {
+  _conductoresRef.child(nombre).update({
+    'informacion/password': newPassword,
+    'informacion/placa': newPlaca,
+  }).then((_) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      const SnackBar(content: Text('Conductor actualizado exitosamente')),
+    );
+    // Actualizar la lista después de actualizar un conductor
+    if (mounted) { // Agregar esta línea para verificar si el widget está montado
+      setState(() {});
+    }
+  }).catchError((error) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text('Error al actualizar conductor: $error')),
+    );
+  });
+}
+
+
+  void _eliminarConductor(String nombre) {
+    _conductoresRef.child(nombre).remove().then((_) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Conductor eliminado exitosamente')),
+      );
+      // Actualizar la lista después de eliminar un conductor
+      setState(() {});
+    }).catchError((error) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Error al eliminar conductor: $error')),
+      );
+    });
   }
 }
--- lib/pages/notifications.dart
+++ lib/pages/notifications.dart
@@ -1,26 +1,193 @@
 import 'package:flutter/material.dart';
 
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Notificaciones',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const Notifications(),
+    );
+  }
+}
+
 class Notifications extends StatelessWidget {
   const Notifications({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Column(
-        children: <Widget>[
-          Card(
-            child: ListTile(
-              leading: Icon(Icons.notifications_sharp),
-              title: Text('Pasajeros a 300 metros'),
-            ),
-          ),
-          Card(
-            child: ListTile(
-              leading: Icon(Icons.notifications_sharp),
-              title: Text('Pasajeros a 600 metros'),
-            ),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Notificaciones'),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.admin_panel_settings),
+            onPressed: () {
+              showDialog(
+                context: context,
+                builder: (BuildContext context) => const AdminPanelDialog(),
+              );
+            },
           ),
         ],
+      ),
+      body: ListView(
+        children: const <Widget>[
+          SectionTitle(title: 'Notificaciones para Usuarios'),
+          NotificationCard(
+            message: 'Pasajeros a 300 metros',
+            iconColor: Colors.green,
+          ),
+          NotificationCard(
+            message: 'Pasajeros a 600 metros',
+            iconColor: Colors.yellow,
+          ),
+          SectionTitle(title: 'Notificaciones para Conductores'),
+          NotificationCard(
+            message: 'Mantenimiento programado',
+            iconColor: Colors.green,
+          ),
+          NotificationCard(
+            message: 'Vehículo averiado',
+            iconColor: Colors.red,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class NotificationCard extends StatelessWidget {
+  final String message;
+  final Color iconColor;
+
+  const NotificationCard({
+    required this.message,
+    required this.iconColor,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        leading: Icon(
+          Icons.notifications_sharp,
+          color: iconColor,
+        ),
+        title: Text(message),
+      ),
+    );
+  }
+}
+
+class SectionTitle extends StatelessWidget {
+  final String title;
+
+  const SectionTitle({required this.title, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Text(
+        title,
+        style: const TextStyle(
+          fontSize: 18,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    );
+  }
+}
+
+class AdminPanelDialog extends StatefulWidget {
+  const AdminPanelDialog({super.key});
+
+  @override
+  _AdminPanelDialogState createState() => _AdminPanelDialogState();
+}
+
+class _AdminPanelDialogState extends State<AdminPanelDialog> {
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _messageController = TextEditingController();
+  String _notificationType = 'Usuario';
+  String _notificationStatus = 'Verde';
+
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              TextFormField(
+                controller: _messageController,
+                decoration: const InputDecoration(labelText: 'Mensaje'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Por favor ingrese un mensaje';
+                  }
+                  return null;
+                },
+              ),
+              DropdownButtonFormField<String>(
+                value: _notificationType,
+                decoration: const InputDecoration(labelText: 'Notificacion para:'),
+                items: <String>['Usuario', 'Conductor']
+                    .map<DropdownMenuItem<String>>((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+                onChanged: (String? newValue) {
+                  setState(() {
+                    _notificationType = newValue!;
+                  });
+                },
+              ),
+              DropdownButtonFormField<String>(
+                value: _notificationStatus,
+                decoration: const InputDecoration(labelText: 'Estado de Notificación:'),
+                items: <String>['Verde', 'Amarillo', 'Rojo']
+                    .map<DropdownMenuItem<String>>((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+                onChanged: (String? newValue) {
+                  setState(() {
+                    _notificationStatus = newValue!;
+                  });
+                },
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    // Aquí se podría agregar la lógica para enviar la notificación
+                    // por ejemplo, agregar la notificación a una lista o enviarla a un servidor
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(content: Text('Notificación enviada')),
+                    );
+                    Navigator.of(context).pop();
+                  }
+                },
+                child: const Text('Enviar Notificación'),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
