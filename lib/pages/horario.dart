c68fc6f8e80c8fbf7ff224b4b3f4b2cbf90f0ced
RafaelReynoso <rafael.reynoso@tecsup.edu.pe>
2024-06-03 13:04:47 -0500

    actualizando horario

--- lib/pages/horario.dart
+++ lib/pages/horario.dart
@@ -3,9 +3,11 @@ import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:file_picker/file_picker.dart';
+import 'package:pdf/pdf.dart';
+import 'package:pdf/widgets.dart' as pw;
 
 class Horario extends StatefulWidget {
-  const Horario({super.key});
+  const Horario({Key? key}) : super(key: key);
 
   @override
   State<Horario> createState() => _HorarioState();
@@ -53,7 +55,7 @@ class _HorarioState extends State<Horario> {
               onPressed: () async {
                 FilePickerResult? result = await FilePicker.platform.pickFiles(
                   type: FileType.custom,
-                  allowedExtensions: ['xlsx', 'xls'],
+                  allowedExtensions: ['pdf'],
                 );
 
                 if (result != null) {
@@ -76,7 +78,7 @@ class _HorarioState extends State<Horario> {
                   Icon(Icons.upload_file, size: 18, color: Colors.green),
                   SizedBox(width: 8), // Espacio entre el ícono y el texto
                   Text(
-                    'Seleccionar Horario',
+                    'Seleccionar Horario (PDF)',
                     style: TextStyle(
                       color: Colors.green,
                       fontSize: 18,
@@ -117,14 +119,32 @@ class _HorarioState extends State<Horario> {
             ElevatedButton(
               onPressed: () async {
                 if (_isFileLoaded) {
+                  final pdf = pw.Document();
+
+                  pdf.addPage(
+                    pw.Page(
+                      build: (context) {
+                        return pw.Center(
+                          child: pw.Text(
+                            'Contenido del horario aquí', // Aquí deberías incluir el contenido de tu horario
+                            style: pw.TextStyle(fontSize: 20),
+                          ),
+                        );
+                      },
+                    ),
+                  );
+
+                  final pdfBytes = await pdf.save();
+                  final pdfFileName = 'horario.pdf'; // Nombre del archivo PDF
+
                   Reference storageRef = FirebaseStorage.instance
                       .ref()
                       .child('horarios')
-                      .child(_fileName!);
-                  await storageRef.putFile(File(_filePath!));
+                      .child(pdfFileName);
+                  await storageRef.putData(pdfBytes);
 
                   String fileUrl = await FirebaseStorage.instance
-                      .ref('horarios/$_fileName')
+                      .ref('horarios/$pdfFileName')
                       .getDownloadURL();
                   await FirebaseFirestore.instance
                       .collection('horarios')
@@ -135,34 +155,37 @@ class _HorarioState extends State<Horario> {
                     context: context,
                     builder: (BuildContext context) {
                       return AlertDialog(
-                      title: const Row(
-                        children: [
-                        Icon(
-                          Icons.check_circle,
-                          color: Colors.green,
+                        title: const Row(
+                          children: [
+                            Icon(
+                              Icons.check_circle,
+                              color: Colors.green,
+                            ),
+                            SizedBox(width: 10),
+                            Text(
+                              'Éxito',
+                              style: TextStyle(
+                                color: Colors.green,
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
                         ),
-                        SizedBox(width: 10),
-                        Text(
-                          'Éxito',
-                          style: TextStyle(
-                          color: Colors.green,
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold,
+                        content: const Text('Horario publicado con éxito.'),
+                        actions: [
+                          TextButton(
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                            child: const Text(
+                              'Aceptar',
+                              style: TextStyle(
+                                color: Colors.green,
+                              ),
+                            ),
                           ),
-                        ),
                         ],
-                      ),
-                      content: const Text('Horario publicado con éxito.'),
-                      actions: [
-                        TextButton(
-                        onPressed: () {
-                          Navigator.of(context).pop();
-                        },
-                        child: const Text('Aceptar',style: TextStyle(
-                          color: Colors.green,
-                          ),),
-                        ),
-                      ],
                       );
                     },
                   );
@@ -181,7 +204,7 @@ class _HorarioState extends State<Horario> {
                   Icon(Icons.send, size: 18, color: Colors.green),
                   SizedBox(width: 8), // Espacio entre el ícono y el texto
                   Text(
-                    'Publicar Horario',
+                    'Publicar Horario (PDF)',
                     style: TextStyle(
                       color: Colors.green,
                       fontSize: 18,
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,6 +8,7 @@ import Foundation
 import cloud_firestore
 import firebase_core
 import firebase_database
+import firebase_messaging
 import firebase_storage
 import flutter_image_compress_macos
 import location
@@ -17,6 +18,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseDatabasePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseDatabasePlugin"))
+  FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
   FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
   FlutterImageCompressMacosPlugin.register(with: registry.registrar(forPlugin: "FlutterImageCompressMacosPlugin"))
   LocationPlugin.register(with: registry.registrar(forPlugin: "LocationPlugin"))
